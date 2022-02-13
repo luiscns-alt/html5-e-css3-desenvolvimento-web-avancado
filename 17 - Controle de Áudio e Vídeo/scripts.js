fcb4f6fa3b8d0e5a92ca56927c6eaef53bd29f5f
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-13 19:10:34 -0300

    controle de áudio e video

---
+++ 17 - Controle de Áudio e Vídeo/scripts.js
@@ -0,0 +1,40 @@
+var myVideo = document.getElementById("myVideo"),
+  logo = document.getElementsByClassName("logo")[0],
+  adTimer = null;
+
+myVideo.addEventListener("playing", function () {
+  showAds();
+});
+
+myVideo.addEventListener("pause", function () {
+  stopAds();
+});
+
+function showAds() {
+  stopAds();
+  adTimer = setInterval(() => {
+    logo.style.display = "block";
+    setTimeout(() => {
+      logo.style.display = "none";
+    }, 300);
+  }, 5000);
+}
+
+function stopAds() {
+  clearInterval(adTimer);
+}
+
+function setVideoTime(position) {
+  myVideo.currentTime = position;
+  myVideo.play();
+}
+
+function setSpeed(direction) {
+  if (direction === "up") {
+    myVideo.playbackRate += 0.5;
+  } else if (direction === "down") {
+    myVideo.playbackRate -= 0.25;
+  } else {
+    myVideo.playbackRate = 1;
+  }
+}
